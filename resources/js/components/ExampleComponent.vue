4ae0673292550aae0341dc0ce549064492d4f96d
Adan Cruz <acruzchoferpro.com>
2018-11-15 16:03:15 +0000

    primer commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <input type="text" class="form-control" v-bind:value="value" v-on:input="$emit('input', $event.target.value)">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+        mounted() {
+            console.log('Component mounted.');
+            this.ready();
+        },
+        methods: {
+            ready(){
+                console.log('ready');
+            }
+        }
+    }
+</script>
---
+++ resources/js/components/ParentComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <example-component v-model="searchText"></example-component>
+            <example-component v-model="searchText2"></example-component>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component parent mounted.');
+        },
+        data() {
+            return {
+                searchText: '',
+                searchText2: '',
+            }
+        }
+    }
+</script>
